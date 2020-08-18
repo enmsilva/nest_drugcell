d52f4aa6078325e7a9b8d2e750c3f0faee622380
Akshat Singhal <a2singhal@ucsd.edu>
2020-08-18 15:00:08 -0700

    Code refactoring

--- scripts/commandline_train.sh
+++ scripts/commandline_train.sh
@@ -17,11 +17,11 @@ mkdir -p $modeldir
 
 cudaid=$2
 
-pyScript="${homedir}/code/cross_validate.py"
+pyScript="${homedir}/code/train_drugcell.py"
 
 source activate pytorch3drugcell
 
 python -u $pyScript -onto $ontfile -gene2id $gene2idfile -drug2id $drug2idfile \
-	-cell2id $cell2idfile -train $traindatafile -model $modeldir -cuda $cudaid \
-	-genotype $mutationfile -fingerprint $drugfile -genotype_hiddens 6 \
-	-drug_hiddens '100,50,6' -final_hiddens 6 -batchsize 5000 -blackboxnn 0 > train.log
+	-cell2id $cell2idfile -train $traindatafile -genotype $mutationfile -fingerprint $drugfile \
+	-genotype_hiddens 6 -drug_hiddens '100,50,6' -final_hiddens 6 -model $modeldir \ 
+	-cuda $cudaid -batchsize 5000 -epoch 200 -blackboxnn 0 -cross_val 0 > train.log
