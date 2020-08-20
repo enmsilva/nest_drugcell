33bdad8ea159c062e4412ae518cf65781f1819f6
Akshat Singhal <a2singhal@ucsd.edu>
2020-08-20 14:06:02 -0700

    bug fixes

--- scripts/commandline_train.sh
+++ scripts/commandline_train.sh
@@ -3,7 +3,7 @@ homedir=$1
 gene2idfile="${homedir}/data/gene2ind.txt"
 cell2idfile="${homedir}/data/cell2ind.txt"
 drug2idfile="${homedir}/data/drug2ind.txt"
-ontfile="${homedir}/data/drugcell_ont.txt"
+ontfile="${homedir}/data/drugcell_ont_bb.txt"
 mutationfile="${homedir}/data/cell2mutation.txt"
 drugfile="${homedir}/data/drug2fingerprint.txt"
 traindatafile="${homedir}/data/drugcell_train.txt"
@@ -23,5 +23,5 @@ source activate pytorch3drugcell
 
 python -u $pyScript -onto $ontfile -gene2id $gene2idfile -drug2id $drug2idfile \
 	-cell2id $cell2idfile -train $traindatafile -genotype $mutationfile -fingerprint $drugfile \
-	-genotype_hiddens 6 -drug_hiddens '100,50,6' -final_hiddens 6 -model $modeldir \ 
+	-genotype_hiddens 6 -drug_hiddens '100,50,6' -final_hiddens 6 -model $modeldir \
 	-cuda $cudaid -batchsize 5000 -epoch 200 -cross_val 0 > train.log
