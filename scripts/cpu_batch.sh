b59f17ddc8ed6cea495ff40c2affb5ccb5f2d22b
Akshat Singhal <asinghal@nrnb-login-01.cm.cluster>
2021-01-30 00:36:57 -0800

    script changes for the new cluster

--- scripts/cpu_batch.sh
+++ scripts/cpu_batch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=RLIPP_NeST_DrugCell
 #SBATCH --output=cpu_out.log
-#SBATCH --nodelist=nrnb-5-4
+#SBATCH --partition=nrnb-compute
+#SBATCH --nodelist=nrnb-cn-12
 #SBATCH --mem=64G
 
 homedir="/cellar/users/asinghal/Workspace/nest_drugcell"
