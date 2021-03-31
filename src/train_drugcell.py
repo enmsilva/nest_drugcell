772a9a9e3a71e07d2748172a39914fdd5bc25086
Akshat Singhal <a2singhal@ucsd.edu>
2021-03-30 17:07:33 -0700

    More spacing error

--- src/train_drugcell.py
+++ src/train_drugcell.py
@@ -274,24 +274,24 @@ def main():
 
 		study = optuna.create_study(direction="maximize")
 
-	    study.optimize(lambda trial: exec_training(trial, opt), n_trials=200)
+		study.optimize(lambda trial: exec_training(trial, opt), n_trials=200)
 
 		pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
-	    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+		complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
 
-	    print("Study statistics: ")
-	    print("  Number of finished trials: ", len(study.trials))
-	    print("  Number of pruned trials: ", len(pruned_trials))
-	    print("  Number of complete trials: ", len(complete_trials))
+		print("Study statistics: ")
+		print("  Number of finished trials: ", len(study.trials))
+		print("  Number of pruned trials: ", len(pruned_trials))
+		print("  Number of complete trials: ", len(complete_trials))
 
-	    print("Best trial:")
-	    trial = study.best_trial
+		print("Best trial:")
+		trial = study.best_trial
 
-	    print("  Value: ", trial.value)
+		print("  Value: ", trial.value)
 
-	    print("  Params: ")
-	    for key, value in trial.params.items():
-	        print("    {}: {}".format(key, value))
+		print("  Params: ")
+		for key, value in trial.params.items():
+			print("    {}: {}".format(key, value))
 
 
 if __name__ == "__main__":
