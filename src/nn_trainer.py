2dcb1c5d6d6f0fa444799e86b50a887a7185abf8
Akshat Singhal <a2singhal@ucsd.edu>
2021-05-04 17:42:01 -0700

    Mystery part 3

--- src/nn_trainer.py
+++ src/nn_trainer.py
@@ -118,10 +118,10 @@ class NNTrainer():
 
 		self.finalize_variance()
 		mean_variance_map, mean_viann_score_map = self.calc_feature_importance(term_mask_map)
-		print(mean_variance_map)
 		mean_variance_map = {g:v for g,v in sorted(mean_variance_map.items(), key=lambda item:item[1], reverse=True)}
 		mean_viann_score_map = {g:sc for g,sc in sorted(mean_viann_score_map.items(), key=lambda item:item[1], reverse=True)}
 		mutations_per_gene = np.count_nonzero(self.data_wrapper.cell_features.transpose() == 1, axis=1)
+		print(mean_variance_map)
 		for gene, score in mean_viann_score_map.items():
 			mut_freq = mutations_per_gene[self.data_wrapper.gene_id_mapping[gene]]
 			print("Gene %s\t Mutation_frequency %.4f\t Variance %.4f\t VIANN_score %.4f" % (gene, mut_freq, mean_variance_map[gene], score))
@@ -170,5 +170,4 @@ class NNTrainer():
 		for i, gene in enumerate(self.data_wrapper.gene_id_mapping.keys()):
 			mean_variance_map[gene] = variance_sum[i].item()/viann_freq[i].item()
 			mean_viann_score_map[gene] = viann_scores[i].item()/viann_freq[i].item()
-		print(mean_variance_map)
 		return mean_variance_map, mean_viann_score_map
